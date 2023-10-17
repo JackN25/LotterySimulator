31f386bcb40b3d7c0f37c002dac8f79062397127
BT_4N2_33 <@>
2023-10-17 13:58:18 -0400

    .

---
+++ src/Lottery.java
@@ -0,0 +1,23 @@
+public class Lottery {
+    //Variables
+    private int lotteryNumber;
+    private int winnings;
+    //Constructor
+    public Lottery() {
+        lotteryNumber = (int) (Math.random() * 99);
+    }
+
+    //Returns earnings
+    public int determineWinnings(int userNumber) {
+        if (userNumber == lotteryNumber) {
+            winnings = 100;
+        } else if ((userNumber % 10 == lotteryNumber / 10) && (userNumber / 10 == lotteryNumber % 10)) {
+            winnings = 10;
+        } else if ((userNumber % 10 == lotteryNumber % 10) || (userNumber % 10 == lotteryNumber / 10) || (userNumber / 10 == lotteryNumber % 10) || (userNumber / 10 == lotteryNumber /10)) {
+            winnings = 3;
+        } else {
+            winnings = 0;
+        }
+        return winnings;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lottery lottery = new Lottery();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String userInputPlaceholder = s.nextLine();
+        int userInput = Integer.parseInt(userInputPlaceholder);
+        int winnings = lottery.determineWinnings(userInput);
+        if (winnings == 100) {
+            System.out.println("Exact Match: You win $100!");
+        } else if (winnings == 10) {
+            System.out.println("Match all digits but not correct order: You win $10!");
+        } else if (winnings == 3) {
+            System.out.println("Match one digit: You win $3!");
+        } else {
+            System.out.println("No matching numbers, you win nothing :(");
+        }
     }
 }
